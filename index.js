818b28213c313e21ea0f041cc81110a146bc7de0
Eamon Sheil <Eamondsheil@gmail.com>
2022-02-23 11:57:29 -0500

    commit tree 566a1bc47262fd47d01c4fbcab8c06e62abdb478

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun("bathe my dog");
+
+const mondayWork = function (thing = "go to the office") {
+    return `This Monday, I will ${thing}.`;
+}
+mondayWork("work from home");
+
+function wrapAdjective(str = "*") {
+    return function(param = "special") {
+        return `You are ${str}${param}${str}!`;
+    }
+}
